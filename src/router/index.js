498ae875d9dcd30bbb1481af9d3489b3182ebb2b
zoe19940213 <zoe19940213@gmail.com>
2022-05-12 02:38:23 +0800

    upload icons & add icons variables

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import SignIn from '../views/SignIn'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'root',
     redirect: '/signIn'
@@ -18,91 +17,104 @@ const routes = [
   {
     path: '/register',
     name: 'register',
-    component: () => import('../views/Register')
+    component: () =>
+      import ('../views/Register')
   },
   {
     path: '/home',
     name: 'home-page',
-    component: () => import('../views/HomePage')
+    component: () =>
+      import ('../views/HomePage')
   },
   {
     path: '/posts/:id',
     name: 'single-post',
-    component: () => import('../views/SinglePost')
+    component: () =>
+      import ('../views/SinglePost')
   },
   {
     path: '/admin',
     name: 'admin-root',
-    component: () => import('../views/Admin'),
+    component: () =>
+      import ('../views/Admin'),
     redirect: '/admin/signIn',
-    children: [
-      {
+    children: [{
         path: 'users',
         name: 'admin-users',
-        component: () => import('../components/AdminUserList')
+        component: () =>
+          import ('../components/AdminUserList')
       },
       {
         path: 'posts',
         name: 'admin-posts',
-        component: () => import('../components/AdminPostList')
+        component: () =>
+          import ('../components/AdminPostList')
       },
     ]
   },
   {
     path: '/admin/signIn',
     name: 'admin-sign-in',
-    component: () => import('../views/AdminSignIn')
+    component: () =>
+      import ('../views/AdminSignIn')
   },
   {
     path: '/users/:userAccount',
     name: 'personal-page-root',
-    component: () => import('../views/PersonalPage'),
+    component: () =>
+      import ('../views/PersonalPage'),
     redirect: '/users/:userAccount/posts',
-    children: [
-      {
+    children: [{
         path: 'posts',
         name: 'personal-page-posts',
-        component: () => import('../components/PostBlockShort')
+        component: () =>
+          import ('../components/PostBlockShort')
       },
       {
         path: 'replies',
         name: 'personal-page-replies',
-        component: () => import('../components/ReplyBlock')
+        component: () =>
+          import ('../components/ReplyBlock')
       },
       {
         path: 'liked',
         name: 'personal-page-liked',
-        component: () => import('../components/PostBlockShort')
-      }, 
+        component: () =>
+          import ('../components/PostBlockShort')
+      },
     ]
   },
   {
     path: '/users/:userAccount/follow',
     name: 'follow-page-root',
-    component: () => import ('../views/FollowPage'),
+    component: () =>
+      import ('../views/FollowPage'),
     redirect: '/users/:userAccount/follow/followers',
-    children: [
-      {
+    children: [{
         path: 'followers',
         name: 'follow-page-followers',
-        component: () => import('../components/FollowUsers')
+        component: () =>
+          import ('../components/FollowUsers')
       },
       {
         path: 'followings',
         name: 'follow-page-following',
-        component: () => import('../components/FollowUsers')
+        component: () =>
+          import ('../components/FollowUsers')
       },
     ]
   },
   {
     path: '/users/:userAccount/setting',
     name: 'setting-page',
-    component: () => import('../views/SettingPage')
+    component: () =>
+      import ('../views/SettingPage')
   },
   {
     path: '*',
     name: '404',
-    component: () => import('../views/PageNotFound')
+    component: () =>
+      import ('../views/PageNotFound')
   }
 ]
 

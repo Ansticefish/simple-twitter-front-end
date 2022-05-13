2733d656f0ac7439d84f675c1d6712043ce850b6
zoe19940213 <zoe19940213@gmail.com>
2022-05-14 00:56:31 +0800

    AdminSideBar adn SideBar complete

--- src/store/index.js
+++ src/store/index.js
@@ -4,32 +4,36 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-        currentUser: {
-            id: -1,
-            account: 'root',
-            name: 'root',
-            email: 'root@example.com',
-            avatar: '',
-            role: '',
-        },
-        isAuthenticated: true,
-        token: '0516'
-
+  state: {
+    currentUser: {
+      id: -1,
+      account: 'root',
+      name: 'root',
+      email: 'root@example.com',
+      avatar: '',
+      role: 'admin',
     },
-    getters: {},
-    mutations: {
-        setCurrentUser(state, currentUser) {
-            state.currentUser = {
-                ...state.currentUser,
-                ...currentUser
-            }
-            state.isAuthenticated = true
-            state.token = localStorage.getItem('token')
-        }
+    isAuthenticated: true,
+    token: '0516'
+  },
+  getters: {},
+  mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        ...currentUser
+      }
+      state.isAuthenticated = true
+      state.token = localStorage.getItem('token')
     },
-    actions: {
-        // add fetch API 
-    },
-    modules: {}
+    revokeAuthentication(state) {
+      state.currentUser = {}
+      state.isAuthenticated = false
+      localStorage.removeItem('token')
+    }
+  },
+  actions: {
+    // add fetch API 
+  },
+  modules: {}
 })

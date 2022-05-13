4fc32ea72431c141b819770f048cd0c280f84b24
Ansticefish <ansticefish0106@gmail.com>
2022-05-13 15:58:40 +0800

    Set up Register, SignIn, AdminSignIn, Setting pages UI and function

--- src/router/index.js
+++ src/router/index.js
@@ -1,125 +1,200 @@
+import { Toast, ToastIcon } from '../utils/helpers'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 import SignIn from '../views/SignIn'
 
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'root',
-    redirect: '/signIn'
-  },
-  {
-    path: '/signIn',
-    name: 'sign-in',
-    component: SignIn,
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: () =>
-      import ('../views/Register')
-  },
-  {
-    path: '/home',
-    name: 'home-page',
-    component: () =>
-      import ('../views/HomePage')
-  },
-  {
-    path: '/posts/:id',
-    name: 'single-post',
-    component: () =>
-      import ('../views/SinglePost')
-  },
-  {
-    path: '/admin',
-    name: 'admin-root',
-    component: () =>
-      import ('../views/Admin'),
-    redirect: '/admin/signIn',
-    children: [{
-        path: 'users',
-        name: 'admin-users',
+        path: '/',
+        name: 'root',
+        redirect: '/signIn'
+    },
+    {
+        path: '/signIn',
+        name: 'sign-in',
+        component: SignIn,
+    },
+    {
+        path: '/register',
+        name: 'register',
         component: () =>
-          import ('../components/AdminUserList')
-      },
-      {
-        path: 'posts',
-        name: 'admin-posts',
+            import ('../views/Register')
+    },
+    {
+        path: '/home',
+        name: 'home-page',
         component: () =>
-          import ('../components/AdminPostList')
-      },
-    ]
-  },
-  {
-    path: '/admin/signIn',
-    name: 'admin-sign-in',
-    component: () =>
-      import ('../views/AdminSignIn')
-  },
-  {
-    path: '/users/:userAccount',
-    name: 'personal-page-root',
-    component: () =>
-      import ('../views/PersonalPage'),
-    redirect: '/users/:userAccount/posts',
-    children: [{
-        path: 'posts',
-        name: 'personal-page-posts',
+            import ('../views/HomePage')
+    },
+    {
+        path: '/posts/:id',
+        name: 'single-post',
         component: () =>
-          import ('../components/PostBlockShort')
-      },
-      {
-        path: 'replies',
-        name: 'personal-page-replies',
+            import ('../views/SinglePost')
+    },
+    {
+        path: '/admin',
+        name: 'admin-root',
         component: () =>
-          import ('../components/ReplyBlock')
-      },
-      {
-        path: 'liked',
-        name: 'personal-page-liked',
+            import ('../views/Admin'),
+        redirect: '/admin/signIn',
+        children: [{
+                path: 'users',
+                name: 'admin-users',
+                component: () =>
+                    import ('../components/AdminUserList')
+            },
+            {
+                path: 'posts',
+                name: 'admin-posts',
+                component: () =>
+                    import ('../components/AdminPostList')
+            },
+        ]
+    },
+    {
+        path: '/admin/signIn',
+        name: 'admin-sign-in',
         component: () =>
-          import ('../components/PostBlockShort')
-      },
-    ]
-  },
-  {
-    path: '/users/:userAccount/follow',
-    name: 'follow-page-root',
-    component: () =>
-      import ('../views/FollowPage'),
-    redirect: '/users/:userAccount/follow/followers',
-    children: [{
-        path: 'followers',
-        name: 'follow-page-followers',
+            import ('../views/AdminSignIn')
+    },
+    {
+        path: '/users/:userAccount',
+        name: 'personal-page-root',
         component: () =>
-          import ('../components/FollowUsers')
-      },
-      {
-        path: 'followings',
-        name: 'follow-page-following',
+            import ('../views/PersonalPage'),
+        redirect: '/users/:userAccount/posts',
+        children: [{
+                path: 'posts',
+                name: 'personal-page-posts',
+                component: () =>
+                    import ('../components/PostBlockShort')
+            },
+            {
+                path: 'replies',
+                name: 'personal-page-replies',
+                component: () =>
+                    import ('../components/ReplyBlock')
+            },
+            {
+                path: 'liked',
+                name: 'personal-page-liked',
+                component: () =>
+                    import ('../components/PostBlockShort')
+            },
+        ]
+    },
+    {
+        path: '/users/:userAccount/follow',
+        name: 'follow-page-root',
         component: () =>
-          import ('../components/FollowUsers')
-      },
-    ]
-  },
-  {
-    path: '/users/:userAccount/setting',
-    name: 'setting-page',
-    component: () =>
-      import ('../views/SettingPage')
-  },
-  {
-    path: '*',
-    name: '404',
-    component: () =>
-      import ('../views/PageNotFound')
-  }
+            import ('../views/FollowPage'),
+        redirect: '/users/:userAccount/follow/followers',
+        children: [{
+                path: 'followers',
+                name: 'follow-page-followers',
+                component: () =>
+                    import ('../components/FollowUsers')
+            },
+            {
+                path: 'followings',
+                name: 'follow-page-following',
+                component: () =>
+                    import ('../components/FollowUsers')
+            },
+        ]
+    },
+    {
+        path: '/setting/:userAccount',
+        name: 'setting-page',
+        component: () =>
+            import ('../views/SettingPage')
+    },
+    {
+        path: '*',
+        name: '404',
+        component: () =>
+            import ('../views/PageNotFound')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    //get currentUser data from vuex 
+    const token = localStorage.getItem('token')
+    let isAuthenticated = store.state.isAuthenticated
+    let role = store.state.currentUser.role
+
+    //if token is different
+    if (token && token !== store.state.token) {
+        // ask API to fetch currentUser again, and set isAuthenticated value based on return value
+        isAuthenticated = false
+        localStorage.removeItem('token')
+    }
+
+    // isAuthenticated: false
+    const pagesWithoutAuthentication = ['register', 'sign-in', 'admin-sign-in']
+    const adminPages = ['admin-users', 'admin-posts']
+
+    if (!isAuthenticated &&
+        !pagesWithoutAuthentication.includes(to.name)) {
+        if (from.name === 'sign-in') {
+            return
+        } else {
+            router.push({ name: 'sign-in' })
+        }
+    }
+
+    // isAuthenticated: true => users can't see sign-in-related pages
+    if (isAuthenticated &&
+        role == 'admin' &&
+        pagesWithoutAuthentication.includes(to.name)) {
+        from.name === 'admin-users' ?
+            router.push('') : router.push({
+                name: 'admin-users'
+            })
+    } else if (isAuthenticated &&
+        role !== 'admin' &&
+        pagesWithoutAuthentication.includes(to.name)) {
+        from.name === 'home-page' ?
+            router.push('') : router.push({
+                name: 'home-page'
+            })
+    }
+
+
+    // separate signed-in users with roles
+    if (isAuthenticated &&
+        role === 'admin' &&
+        !pagesWithoutAuthentication.includes(to.name) &&
+        !adminPages.includes(to.name)) {
+        Toast.fire({
+            title: '此為後台帳號，無前台權限',
+            html: ToastIcon.redCrossHtml,
+        })
+        router.push({
+            name: 'admin-users'
+        })
+    } else if (isAuthenticated &&
+        role !== 'admin' &&
+        !pagesWithoutAuthentication.includes(to.name) &&
+        adminPages.includes(to.name)) {
+        Toast.fire({
+            title: '此為前台帳號，無後台權限',
+            html: ToastIcon.redCrossHtml,
+        })
+        router.push({
+            name: 'home-page'
+        })
+    }
+
+    next()
+
 })
 
 export default router
--- src/views/AdminSignIn.vue
+++ src/views/AdminSignIn.vue
@@ -16,7 +16,7 @@
           後台登入
         </h3>
         <form 
-          @submit.stop.prevent=""
+          @submit.stop.prevent="handleSubmit"
           class="admin__form"
         >
           <div 
@@ -27,9 +27,9 @@
             <label for="admin__form__wrapper__email">
             Email
             </label>
-            <input type="text" 
+            <input type="email" 
             v-model="email"
-            name="admin__form__wrapper__email"
+            name="email"
             id="admin__form__wrapper__email" 
             placeholder="請輸入Email" 
             required
@@ -44,9 +44,9 @@
             <label for="admin__form__wrapper__password">
             密碼
             </label>
-            <input type="text" 
+            <input type="password" 
             v-model="password"
-            name="admin__form__wrapper__password"
+            name="password"
             id="admin__form__wrapper__password" 
             placeholder="請輸入密碼" 
             required
@@ -55,8 +55,9 @@
           <button   
            class="admin__form__btn--submit
            mt-8"
+           :disabled="isProcessing"
           >
-            登入
+            {{ isProcessing? '處理中': '登入'}}
           </button>
         </form>
         <ul 
@@ -78,14 +79,78 @@
 </template>
 
 <script>
+import { Toast, ToastIcon } from '../utils/helpers'
+
+const dummyAdmin = {
+  email: 'admin@example.com',
+  password: '12345678'
+}
+
+const dummyData = {
+  status: 'success',
+  message: 'You have signed in!',
+  token: '0777',
+  currentUser: {
+    id: 66,
+    account: 'admin',
+    name: 'admin',
+    avatar: '@/assets/icons/bell.png',
+    isAdmin: 'admin',
+  }
+}
+
+
 export default {
-  name: 'Adminadmin',
+  name: 'AdminSignIn',
   data () {
     return {
       email: '',
       password: '',
+      isProcessing: false
     }
   },
+  methods: {
+    handleSubmit () {
+
+      this.isProcessing = true 
+      // avoid empty data
+      if(!this.email.trim() || !this.password.trim()) {
+        Toast.fire({
+          title: 'Email、密碼不可空白！',
+          html: ToastIcon.redCrossHtml 
+        })
+         this.isProcessing = false 
+        return
+      }
+
+      // add API
+      // handle errors from server
+      if(dummyData.status !== 'success') {
+        throw new Error(dummyData.message)
+      }
+      
+      // sign in successfully or not
+      if ( this.email === dummyAdmin.email && this.password === dummyAdmin.password ) {
+        // if success, store token
+        localStorage.setItem('token', dummyData.token)
+        Toast.fire({
+          title: '登入成功',
+          html: ToastIcon.greenCheckHtml
+        })
+      } else {
+        Toast.fire({
+          title: '登入失敗，請重新確認',
+          html: ToastIcon.redCrossHtml 
+        })
+        this.isProcessing = false
+        return
+      }
+
+      // if successfully sign in 
+      this.$store.commit('setCurrentUser', dummyData.currentUser)
+      this.$router.push('/admin/users')
+    } 
+  }
 }
 </script>
 

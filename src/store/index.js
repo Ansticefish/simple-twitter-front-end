966a68257dae2cc35f890c6a245a5755fee51081
Ansticefish <ansticefish0106@gmail.com>
2022-05-14 11:57:12 +0800

    Change store setting

--- src/store/index.js
+++ src/store/index.js
@@ -4,36 +4,38 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    currentUser: {
-      id: -1,
-      account: 'root',
-      name: 'root',
-      email: 'root@example.com',
-      avatar: '',
-      role: 'admin',
+    state: {
+        currentUser: {
+            id: -1,
+            account: 'root',
+            name: 'root',
+            email: 'root@example.com',
+            avatar: '',
+            role: 'admin',
+        },
+        isAuthenticated: true,
+        token: '0516'
     },
-    isAuthenticated: true,
-    token: '0516'
-  },
-  getters: {},
-  mutations: {
-    setCurrentUser(state, currentUser) {
-      state.currentUser = {
-        ...state.currentUser,
-        ...currentUser
-      }
-      state.isAuthenticated = true
-      state.token = localStorage.getItem('token')
+    getters: {},
+    mutations: {
+        setCurrentUser(state, currentUser) {
+            state.currentUser = {
+                ...state.currentUser,
+                ...currentUser
+            }
+            state.isAuthenticated = true
+            state.token = localStorage.getItem('token')
+        },
+        revokeAuthentication(state) {
+            state.currentUser = {}
+            state.isAuthenticated = false
+            localStorage.removeItem('token')
+        }
     },
-    revokeAuthentication(state) {
-      state.currentUser = {}
-      state.isAuthenticated = false
-      localStorage.removeItem('token')
-    }
-  },
-  actions: {
-    // add fetch API 
-  },
-  modules: {}
+    actions: {
+        // add fetch API 
+        // add fetch API 
+        // add fetch API 
+    },
+    modules: {}
 })

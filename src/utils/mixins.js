b8b99aabb015fbee91bad8f90e35a119a980f278
zoe19940213 <zoe19940213@gmail.com>
2022-05-15 00:34:34 +0800

    style variables modified, add 2 mixins

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,65 +1,81 @@
 export const preventInputBlank = {
-    watch: {
-        'account': {
-            handler: function() {
-                if (!this.account.slice(1).trim()) {
-                    this.a.error = true
-                    this.a.warning = '此欄位不可空白'
-                } else if (this.account.slice(1).length > 50) {
-                    this.a.error = true
-                    this.a.warning = '字數超出上限！'
-                } else {
-                    this.a.error = false
-                    this.a.warning = ''
-                }
-            }
-        },
-        'name': {
-            handler: function() {
-                if (!this.name.trim()) {
-                    this.n.error = true
-                    this.n.warning = '此欄位不可空白'
-                } else if (this.name.length > 50) {
-                    this.n.error = true
-                    this.n.warning = '字數超出上限！'
-                } else {
-                    this.n.error = false
-                    this.n.warning = ''
-                }
-            }
-        },
-        'email': {
-            handler: function() {
-                if (!this.email.trim()) {
-                    this.m.error = true
-                    this.m.warning = '此欄位不可空白'
-                } else {
-                    this.m.error = false
-                    this.m.warning = ''
-                }
-            }
-        },
-        'password': {
-            handler: function() {
-                if (!this.password.trim()) {
-                    this.p.error = true
-                    this.p.warning = '此欄位不可空白'
-                } else {
-                    this.p.error = false
-                    this.p.warning = ''
-                }
-            }
-        },
-        'checkPassword': {
-            handler: function() {
-                if (!this.checkPassword.trim()) {
-                    this.cp.error = true
-                    this.cp.warning = '此欄位不可空白'
-                } else {
-                    this.cp.error = false
-                    this.cp.warning = ''
-                }
-            }
-        },
+  watch: {
+    'account': {
+      handler: function() {
+        if (!this.account.slice(1).trim()) {
+          this.a.error = true
+          this.a.warning = '此欄位不可空白'
+        } else if (this.account.slice(1).length > 50) {
+          this.a.error = true
+          this.a.warning = '字數超出上限！'
+        } else {
+          this.a.error = false
+          this.a.warning = ''
+        }
+      }
+    },
+    'name': {
+      handler: function() {
+        if (!this.name.trim()) {
+          this.n.error = true
+          this.n.warning = '此欄位不可空白'
+        } else if (this.name.length > 50) {
+          this.n.error = true
+          this.n.warning = '字數超出上限！'
+        } else {
+          this.n.error = false
+          this.n.warning = ''
+        }
+      }
+    },
+    'email': {
+      handler: function() {
+        if (!this.email.trim()) {
+          this.m.error = true
+          this.m.warning = '此欄位不可空白'
+        } else {
+          this.m.error = false
+          this.m.warning = ''
+        }
+      }
+    },
+    'password': {
+      handler: function() {
+        if (!this.password.trim()) {
+          this.p.error = true
+          this.p.warning = '此欄位不可空白'
+        } else {
+          this.p.error = false
+          this.p.warning = ''
+        }
+      }
+    },
+    'checkPassword': {
+      handler: function() {
+        if (!this.checkPassword.trim()) {
+          this.cp.error = true
+          this.cp.warning = '此欄位不可空白'
+        } else {
+          this.cp.error = false
+          this.cp.warning = ''
+        }
+      }
+    },
+  }
+}
+
+export const accountShow = {
+  filters: {
+    accountShow(account) {
+      return `@${account}`
     }
+  },
+}
+
+export const emptyAvatar = {
+  filters: {
+    emptyAvatar(imgURL) {
+      return imgURL || "https://imgpile.com/images/5S6lFC.png"
+    },
+  }
 }

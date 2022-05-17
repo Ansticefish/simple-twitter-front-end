53d97309f629d33556df227192f85c1410b8177a
zoe19940213 <zoe19940213@gmail.com>
2022-05-17 23:07:29 +0800

    SinglePost style setting

--- src/components/PostBlockFull.vue
+++ src/components/PostBlockFull.vue
@@ -1,10 +1,121 @@
 <template>
-  <h1>PostBlockFull</h1>
+  <div class="post-full">
+    <div class="post p-3">
+      <div class="post__main pb-2">
+        <div class="post__main__info">
+          <img
+            :src="post.TweetUser.avatar"
+            alt=""
+            class="post__main__info__avatar mr-2"
+          />
+          <div class="post__main__info__creater">
+            <p class="post__main__info__creater__name">
+              {{ post.TweetUser.name }}
+            </p>
+            <p class="post__main__info__creater__account">
+              {{ post.TweetUser.account }}
+            </p>
+          </div>
+        </div>
+        <div class="post__main__description my-2">{{ post.description }}</div>
+        <div class="post__main__created-time">
+          {{ post.createdAt | showDate }}
+        </div>
+      </div>
+      <div class="post__footer py-3">
+        <p class="post__footer__comment-count mr-5">
+          <span>{{ post.replyCount }}</span> 回覆
+        </p>
+        <p class="post__footer__like-count">
+          <span>{{ post.likeCount }}</span> 喜歡次數
+        </p>
+      </div>
+      <div class="post__response pt-3 my-1">
+        <div class="post__response__comment"></div>
+        <div class="post__response__like"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import moment from "moment";
 export default {
-  name: 'PostBlockFull',
-}
-
+  name: "PostBlockFull",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {},
+  filters: {
+    showDate(dateTime) {
+      moment.locale("zh-tw");
+      const time = moment(dateTime).format("LT");
+      const date = moment(dateTime).format("LL");
+      const timeZone = moment(dateTime).hour > 12 ? '下午' : '上午'
+      return dateTime ? `${timeZone} ${time}・${date}` : "--";
+    },
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.post {
+  border-bottom: 1px solid $color-tab-line;
+  &__main {
+    border-bottom: 1px solid $color-tab-line;
+    &__info {
+      display: flex;
+      align-items: center;
+      &__avatar {
+        @extend %avatar_;
+      }
+      &__creater {
+        &__name {
+          @extend %name_;
+        }
+        &__account {
+          @extend %account_;
+        }
+      }
+    }
+    &__description {
+      font-size: 24px;
+      line-height: 36px;
+    }
+    &__created-time {
+      @extend %account_;
+      font-weight: 500;
+    }
+  }
+  &__footer {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border-bottom: 1px solid $color-tab-line;
+    &__comment-count,
+    &__like-count {
+      font-size: 19px;
+      line-height: 28px;
+      color: $color-secondary;
+      span {
+        font-family: $number-font;
+        font-size: 19px;
+        font-weight: bold;
+      }
+    }
+  }
+  &__response {
+    &__comment { 
+      @include setIcon(25px, 25px, $icon-reply, $icon-reply){
+        margin-right: 130px;
+      }
+    }
+    &__like {
+      @include setIcon(25px, 25px, $icon-heart, $icon-heart-liked);
+    }
+  }
+}
+</style>
---
+++ src/components/SinglePostHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="post-header">
+    <div @click.prevent.stop="backToHome" class="back-btn">
+      <div class="back-btn__icon"></div>
+      <h5 class="back-btn__descript__name">推文</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+// check post numbers
+
+export default {
+  name: "PersonalPageHeader",
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      tweetsAmount: 25,
+    };
+  },
+  methods: {
+    backToHome() {
+      this.$router.push('/home')
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#post-header{
+  @extend %header_
+}
+.back-btn {
+  width: 100px;
+  display: flex;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.6;
+  }
+  &__icon {
+    @include setIcon(
+      17px,
+      14px,
+      url("../assets/icons/left-arrow.png"),
+      url("../assets/icons/left-arrow.png")
+    ) {
+      margin-right: 20px;
+    }
+  }
+  &__descript {
+    &__name {
+      line-height: 26px;
+    }
+  }
+}
+</style>
